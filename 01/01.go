8d26cf135ee3376d4ac6839f4eff7b8111aa04be
Stepan Hojdar <hojdar.stepan@gmail.com>
2023-12-02 10:47:06 +0100

    Add working line parsing for 1-1

--- 01/01.go
+++ 01/01.go
@@ -1,1 +1,27 @@
 package day_one
+
+import "errors"
+
+func ParseLine(line string) (int, error) {
+	first := -1
+	last := -1
+
+	for _, char := range line {
+		if !(char >= '0' && char <= '9') {
+			continue
+		}
+
+		if first == -1 {
+			first = int(char - '0')
+			last = int(char - '0')
+		} else {
+			last = int(char - '0')
+		}
+	}
+
+	if first == -1 {
+		return -1, errors.New("no numbers found")
+	} else {
+		return 10*first + last, nil
+	}
+}
--- 01/01_test.go
+++ 01/01_test.go
@@ -1,1 +1,36 @@
 package day_one
+
+import "testing"
+
+func TestLineRead(t *testing.T) {
+	t.Run("two numbers", func(t *testing.T) {
+		compare(t, "1abc2", 12)
+		compare(t, "pqr3stu8vwx", 38)
+		compare(t, "a1b2c3d4e5f", 15)
+	})
+
+	t.Run("one number", func(t *testing.T) {
+		input := "treb7uchet"
+		compare(t, input, 77)
+	})
+
+	t.Run("no numbers", func(t *testing.T) {
+		input := "abcd"
+		got, err := ParseLine(input)
+		if err == nil {
+			t.Errorf("wanted error, got nil and value %d", got)
+		}
+	})
+}
+
+func compare(t *testing.T, input string, want int) {
+	got, err := ParseLine(input)
+
+	if err != nil {
+		t.Errorf("number not found")
+	}
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
