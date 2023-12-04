1e239c7f79cfcc62931553e1d06ab629abc7be01
Stepan Hojdar <hojdar.stepan@gmail.com>
2023-12-04 18:00:03 +0100

    Implement 4-2

--- 04/04.go
+++ 04/04.go
@@ -46,7 +46,18 @@ func ParseLine(line string) (cardNumber int, winning map[int]struct{}, picked []
 	return
 }
 
-func main() {
+func GetNumberOfWins(winning map[int]struct{}, picked []int) int {
+	numOfHits := 0
+	for _, n := range picked {
+		_, ok := winning[n]
+		if ok {
+			numOfHits++
+		}
+	}
+	return numOfHits
+}
+
+func SolvePartOne() {
 	file, err := os.Open("input/input")
 	if err != nil {
 		fmt.Println("error opening file")
@@ -58,17 +69,41 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		_, winning, picked := ParseLine(line)
-
-		numOfHits := 0
-		for _, n := range picked {
-			_, ok := winning[n]
-			if ok {
-				numOfHits++
-			}
-		}
+		numOfHits := GetNumberOfWins(winning, picked)
 		if numOfHits > 0 {
 			result += 1 << (numOfHits - 1)
 		}
 	}
 	fmt.Printf("4-1 result=%d\n", result)
+}
+
+func SolvePartTwo() {
+	file, err := os.Open("input/input")
+	if err != nil {
+		fmt.Println("error opening file")
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	result := 0
+	cards := make(map[int]int)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		cardNumber, winning, picked := ParseLine(line)
+		numOfHits := GetNumberOfWins(winning, picked)
+
+		cards[cardNumber] += 1
+		result += cards[cardNumber]
+
+		for i := 1; i <= numOfHits; i++ {
+			cards[cardNumber+i] += cards[cardNumber]
+		}
+	}
+	fmt.Printf("4-2 result=%d\n", result)
+}
+
+func main() {
+	SolvePartOne()
+	SolvePartTwo()
 }
